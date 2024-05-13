3991517c152249b6983b73e0a99c0dd127f4b3d7
Mark Eisner <mark@eisner.uk>
2024-05-13 14:54:17 +0400

    Direct to personal fork

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
-	internal "github.com/thomseddon/traefik-forward-auth/internal"
+	internal "github.com/markeeisner/traefik-forward-auth/internal"
 )
 
 // Main
--- internal/auth.go
+++ internal/auth.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/thomseddon/traefik-forward-auth/internal/provider"
+	"github.com/markeeisner/traefik-forward-auth/internal/provider"
 )
 
 // Request Validation
